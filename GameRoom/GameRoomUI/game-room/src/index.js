b8bf75bf6710c7a12c9dd673cfd649ae069f66f4
caleb1111 <calebchen1997@gmail.com>
2019-03-18 18:23:36 -0400

    clean up

--- GameRoom/GameRoomUI/game-room/src/index.js
+++ GameRoom/GameRoomUI/game-room/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style/index.css';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import SignUpIn from './pages/SignUpIn';
