eff729784a530d77955f8e3dee04af03a4df4585
TianJi Liu <tianjiliu@MacBook-Air-2.local>
2019-03-21 16:57:44 -0400

    removed the wrong gameroomUI folder from repo

--- GameRoomUI/game-room/src/js/Components/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../../style/home.css';
-import LogoSrc from '../../media/title.png';
-
-const Logo = () => (
-    <img src={LogoSrc} alt='logo' className='logo'/>
-  );
-
-export default Logo
